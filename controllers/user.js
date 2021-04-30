4b1b0d03560581eae9f93dc6d57426df8ab1f30f
vikasganiga <codewithvikas@gmail.com>
2021-05-01 00:32:40 +0530

    :heavy_plus_sign: Added getUserById middleware

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,29 @@
 const User = require("../models/User");
-const { genSaltSync, hashSync } = require("bcrypt");\
+const { genSaltSync, hashSync } = require("bcrypt");
 
 const getUserById = (req, res) => {
-  
+  User.findOne({ _id: req.body.userId })
+    .then((user) => {
+      user.password = undefined;
+      res.status(200).json({
+        success: true,
+        data: user,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        success: false,
+        message: "Failed to get User!",
+      });
+    });
 };
 
 const addUser = (req, res) => {
 
 };
+
+module.exports = {
+  getUserById,
+  addUser
+}
